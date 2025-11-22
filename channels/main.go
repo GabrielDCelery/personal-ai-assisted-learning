5d4685b8f4138d535da83ece1660982baf1af309
GaborZeller <gabriel.d.celery@gmail.com>
2025-11-22 22:24:41 +0000

    feat: go channels lesson 8

--- channels/main.go
+++ channels/main.go
@@ -2,33 +2,42 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 const numOfJobs = 5
 
 func main() {
-	jobs := make(chan int, 10)
-	results := make(chan int, 10)
+	var wg sync.WaitGroup
+	jobs := make(chan int)
+	results := make(chan int)
 
 	for workerID := range 3 {
-		go worker(workerID, jobs, results)
+		wg.Add(1)
+		go worker(workerID, jobs, results, &wg)
 	}
 
-	for num := range numOfJobs {
-		jobs <- (num + 1)
-	}
+	go func(wg *sync.WaitGroup, results chan<- int) {
+		wg.Wait()
+		close(results)
 
-	close(jobs)
+	}(&wg, results)
 
-	for range numOfJobs {
-		result := <-results
+	go func(jobs chan<- int) {
+		for num := range numOfJobs {
+			jobs <- (num + 1)
+		}
+
+		close(jobs)
+	}(jobs)
+
+	for result := range results {
 		fmt.Printf("result: %d\n", result)
 	}
-
-	close(results)
 }
 
-func worker(workerID int, jobs <-chan int, results chan<- int) {
+func worker(workerID int, jobs <-chan int, results chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for job := range jobs {
 		fmt.Printf("worker %d is processing job %d\n", workerID, job)
 		results <- 2 * job

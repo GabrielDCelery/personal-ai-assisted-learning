12e8231e56438b2b861756d0a01d12f272c8cf6c
GaborZeller <gabriel.d.celery@gmail.com>
2025-11-22 21:24:05 +0000

    feat: go channels lesson 5

--- channels/main.go
+++ channels/main.go
@@ -6,25 +6,17 @@ import (
 )
 
 func main() {
-	ch1 := make(chan string)
-	ch2 := make(chan string)
+	ch := make(chan string)
 
 	go func(ch chan<- string) {
-		time.Sleep(100 * time.Millisecond)
-		ch <- "from channel 1"
-	}(ch1)
+		time.Sleep(500 * time.Millisecond)
+		ch <- "operation completed"
+	}(ch)
 
-	go func(ch chan<- string) {
-		time.Sleep(200 * time.Millisecond)
-		ch <- "from channel 2"
-	}(ch2)
-
-	for range 2 {
-		select {
-		case msg := <-ch1:
-			fmt.Println(msg)
-		case msg := <-ch2:
-			fmt.Println(msg)
-		}
+	select {
+	case msg := <-ch:
+		fmt.Println(msg)
+	case <-time.After(600 * time.Millisecond):
+		fmt.Println("operation took too long")
 	}
 }

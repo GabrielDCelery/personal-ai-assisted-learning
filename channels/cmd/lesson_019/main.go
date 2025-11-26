b71b7c52872ff6d8380cee3f917f794af05dc3a8
GaborZeller <gabriel.d.celery@gmail.com>
2025-11-26 23:29:17 +0000

    refactor: changed lesson 19 to use error groups to fix race conditions

--- channels/cmd/lesson_019/main.go
+++ channels/cmd/lesson_019/main.go
@@ -4,120 +4,79 @@ import (
 	"context"
 	"fmt"
 	"os/signal"
-	"sync"
 	"syscall"
 	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
 	signalCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
-	ctx, cancel := context.WithCancel(signalCtx)
-	defer cancel()
+	g, ctx := errgroup.WithContext(signalCtx)
 
 	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	generatorChan := generator(ctx, nums)
-	transformChan, transformErrorChan := transform(ctx, generatorChan)
-	saveDoneChan, saveErrorChan := save(ctx, transformChan)
+	generatorChan := generator(ctx, nums, g)
+	transformChan := transform(ctx, generatorChan, g)
+	save(ctx, transformChan, g)
 
-	mergedErrorChans := mergeErrorChannels(ctx, transformErrorChan, saveErrorChan)
-
-	for {
-		select {
-		case err, ok := <-mergedErrorChans:
-			if !ok {
-				mergedErrorChans = nil
-				continue
-			}
-			fmt.Printf("pipeline error: %v\n", err)
-			cancel()
-			return
-		case <-saveDoneChan:
-			fmt.Printf("successfully finished processing\n")
-			return
-		}
+	if err := g.Wait(); err != nil {
+		fmt.Printf("pipeline error: %v\n", err)
+		return
 	}
+
+	fmt.Printf("successfully finished processing\n")
 }
 
-func mergeErrorChannels(ctx context.Context, errChansToMerge ...<-chan error) <-chan error {
-	merged := make(chan error)
-	var wg sync.WaitGroup
-	for _, errChan := range errChansToMerge {
-		wg.Add(1)
-		go func(ch <-chan error) {
-			defer wg.Done()
-			for err := range ch {
-				select {
-				case <-ctx.Done():
-					return
-				case merged <- err:
-				}
-			}
-		}(errChan)
-	}
-	go func() {
-		wg.Wait()
-		close(merged)
-	}()
-	return merged
-}
-
-func generator(ctx context.Context, nums []int) <-chan int {
+func generator(ctx context.Context, nums []int, g *errgroup.Group) <-chan int {
 	outChan := make(chan int)
-	go func() {
+	g.Go(func() error {
 		defer close(outChan)
 		for _, num := range nums {
+			time.Sleep(100 * time.Millisecond)
 			select {
 			case <-ctx.Done():
-				return
-			default:
-				time.Sleep(100 * time.Millisecond)
-				outChan <- num
+				return nil
+			case outChan <- num:
 			}
 		}
-	}()
+		return nil
+	})
 	return outChan
 }
 
-func transform(ctx context.Context, inChan <-chan int) (<-chan int, <-chan error) {
-	errChan := make(chan error)
+func transform(ctx context.Context, inChan <-chan int, g *errgroup.Group) <-chan int {
 	outChan := make(chan int)
 
-	go func() {
+	g.Go(func() error {
 		defer close(outChan)
-		defer close(errChan)
 		for num := range inChan {
+			if num == 6 {
+				return fmt.Errorf("transform error: number %d is invalid", num)
+			}
+			time.Sleep(100 * time.Millisecond)
 			select {
 			case <-ctx.Done():
-				return
-			default:
-				if num == 6 {
-					errChan <- fmt.Errorf("transform error: number %d is invalid", num)
-					return
-				}
-				time.Sleep(100 * time.Millisecond)
-				outChan <- num * 2
+				return nil
+			case outChan <- num * 2:
 			}
 		}
-	}()
+		return nil
+	})
 
-	return outChan, errChan
+	return outChan
 }
 
-func save(ctx context.Context, inChan <-chan int) (<-chan struct{}, <-chan error) {
-	doneChan := make(chan struct{})
-	errChan := make(chan error)
-	go func() {
-		defer close(doneChan)
-		defer close(errChan)
+func save(ctx context.Context, inChan <-chan int, g *errgroup.Group) {
+	g.Go(func() error {
 		for num := range inChan {
 			select {
 			case <-ctx.Done():
-				return
+				return nil
 			default:
 				fmt.Printf("saved: %d\n", num)
 			}
 		}
-	}()
-	return doneChan, errChan
+		return nil
+	})
 }
